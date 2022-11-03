147f387e1c3e255ca60ca0f8bc9d9ce85328b3e0
Nishant-28 <92144362+Nishant-28@users.noreply.github.com>
2022-11-03 17:39:17 +0530

    Add files via upload

---
+++ Going_to_Office.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long int D;
+    cin >> D;
+
+    long long int Oc, Of, Od;
+    long long int Cs, Cb, Cm, Cd;
+
+    cin >> Oc >> Of >> Od;
+    cin >> Cs >> Cb >> Cm >> Cd;
+
+    long long int a = 0;
+    a = Oc + (D - Of) * Od;
+
+    long long int b = 0;
+    b = Cb + (D * Cd) + (D / Cs) * Cm;
+
+    if (a <= b) {
+        cout << "Online Taxi" << endl;
+    }
+    else{
+        cout << "Classic Taxi" << endl;
+    }
+    
+    return 0;
+}
